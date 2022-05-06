a2b26f12e50c329533f930815560029e646b5e21
yeferson <yefersonvelandia4@gmail.com>
2022-05-05 23:55:30 -0500

    Ready footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -156,61 +156,22 @@
                   </div>
                   <!-- Grid column -->
           
-                  <!-- Grid column -->
-                  <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <!-- Links -->
-                    <h6 class="text-uppercase fw-bold mb-4">
-                      Products
-                    </h6>
-                    <p>
-                      <a href="#!" class="text-reset">Angular</a>
-                    </p>
-                    <p>
-                      <a href="#!" class="text-reset">React</a>
-                    </p>
-                    <p>
-                      <a href="#!" class="text-reset">Vue</a>
-                    </p>
-                    <p>
-                      <a href="#!" class="text-reset">Laravel</a>
-                    </p>
-                  </div>
-                  <!-- Grid column -->
-          
-                  <!-- Grid column -->
-                  <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <!-- Links -->
-                    <h6 class="text-uppercase fw-bold mb-4">
-                      Useful links
-                    </h6>
-                    <p>
-                      <a href="#!" class="text-reset">Pricing</a>
-                    </p>
-                    <p>
-                      <a href="#!" class="text-reset">Settings</a>
-                    </p>
-                    <p>
-                      <a href="#!" class="text-reset">Orders</a>
-                    </p>
-                    <p>
-                      <a href="#!" class="text-reset">Help</a>
-                    </p>
-                  </div>
-                  <!-- Grid column -->
-          
+                  
                   <!-- Grid column -->
                   <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                     <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">
-                      Contact
+                      Contacto
                     </h6>
-                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                    <p><i class="fas fa-home me-3"></i> Bogota</p>
                     <p>
                       <i class="fas fa-envelope me-3"></i>
-                      info@example.com
+                      somendezv@correo.udistrital.edu.co
                     </p>
-                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                    <p>
+                      <i class="fas fa-envelope me-3"></i>
+                      yrvelandiaa@correo.udistrital.edu.co
+                    </p>
                   </div>
                   <!-- Grid column -->
                 </div>
