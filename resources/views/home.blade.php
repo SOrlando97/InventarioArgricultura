2a92dd8d178eee93bd3292644bb543781bff2da3
yeferson <yefersonvelandia4@gmail.com>
2022-07-31 18:15:11 -0500

    finished changes in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,38 +3,106 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    {{ __('You are logged in!') }}
-                    <div> 
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <div class="card">
-                        <div class="card-body">
-                          Administrar Usuarios
+        @if (Auth::user()->id_rol==2)
+        <h1 class="text-center text-white">¡ Bienvenido {{ Auth::user()->name }} !</h1>
+            <div class="row mt-5">
+                <div class="col-6">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                          <div class="col-md-4">
+                            <img src="/images/perfil.jpg" class="img-fluid rounded-start" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h3 class="card-title text-center"> <strong>Tipos de productos</strong></h3>
+                              <p class="text-center">En esta opción podrás encontrar todos los tipos de productos que has registrado en 
+                                el sistema, además podrás modificarlos y eliminarlos.
+                              </p>
+                                <div style="text-align: center">
+                                    <a class = "mt-5 btn btn-secondary btn-success botonsito"
+                                    href="{{route('Tipo_producto.index')}}">Ver Productos</a>
+                                </div>
+                            </div>
+                          </div>
                         </div>
                       </div>
                 </div>
-                <div class="col">
-
-                <div class="card">
-                    <div class="card-body">
-                      Administrar Productos
-                    </div>
-                  </div>
-                </div>                  
+                <div class="col-6">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                          <div class="col-md-4">
+                            <img src="/images/perfil.jpg" class="img-fluid rounded-start" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h3 class="card-title text-center"><strong> Listado de usuarios </strong></h3>
+                              <p class="text-center ">En este apartado encontrarás todos los usuarios que están registrados 
+                                en el sistema, en el cual podrás ver sus productos, cambiar su contraseña y así mismo eliminarlos del sistema.
+                              </p>
+                                <div style="text-align: center">
+                                    <a class = "mt-4 btn btn-secondary btn-success botonsito"
+                                    href="{{route('Producto.create')}}">Crear Producto</a>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                </div>
             </div>
+            <div class="card"></div>
+        @endif
+
+
+        <div>
+            @if (Auth::user()->id_rol==1)
+            <h1 class="text-center text-white">¡ Bienvenido {{ Auth::user()->name }} !</h1>
+            <div class="row mt-5">
+                <div class="col-6">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                          <div class="col-md-4">
+                            <img src="/images/perfil.jpg" class="img-fluid rounded-start" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h3 class="card-title text-center"> <strong>Listado de productos</strong></h3>
+                              <p class="text-center">En esta opción podrás encontrar todos los productos que has registrado en 
+                                el sistema, además podrás descargar tus reportes de ventas e ingresos.
+                              </p>
+                                <div style="text-align: center">
+                                    <a class = "mt-2 btn btn-secondary btn-success botonsito"
+                                    href="{{route('Producto.index')}}">Ver Productos</a>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                <div class="col-6">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                          <div class="col-md-4">
+                            <img src="/images/perfil.jpg" class="img-fluid rounded-start" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h3 class="card-title text-center"><strong> Crea un producto </strong></h3>
+                              <p class="text-center mb-4">Empieza a registrar tus productos y saca provecho de las 
+                                funciones que contiene el sistema.
+                              </p>
+                                <div style="text-align: center">
+                                    <a class = "mt-5 btn btn-secondary btn-success botonsito"
+                                    href="{{route('Producto.create')}}">Crear Producto</a>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                </div>
+            </div>
+            <div class="card"></div>
+            @endif
         </div>
     </div>
 </div>
