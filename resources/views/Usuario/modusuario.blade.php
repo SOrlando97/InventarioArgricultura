1d3055512508b2b20fb28ffbed980fafed78c07f
yeferson <yefersonvelandia4@gmail.com>
2022-07-31 15:58:26 -0500

    Change view user

--- resources/views/Usuario/modusuario.blade.php
+++ resources/views/Usuario/modusuario.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
 @section('content')
 
-<section class="text-center">
+<section class="text-center ">
     <!-- Background image -->
-    <div class="mt-4 bg-image" style="
-          height: 200px;
-          "></div>
+    <div class="mt-4 bg-image" style="height: 200px;"></div>
     <!-- Background image -->
   
     <div class="card mx-4 mx-md-5 shadow-5-strong" style="
@@ -14,21 +12,84 @@
           backdrop-filter: blur(30px);
           ">
       <div class="card-body py-5 px-md-5">
-
-
-
-
-      <div class="row d-flex justify-content-center">
-          <div class="col-lg-8">
+      <div class="row ">
+        <div class="col-lg-6">
+            <h2 class="fw-bold ">Editar contraseña</h2>
+            <form  method="POST" action="{{ route('change.password')}}">
+                @csrf
+                <div class="row mb-3 text-center mt-3">
+                    <label class="col-md-3 col-form-label mt-4 " for="current_password" >
+                        {{__('Contraseña Actual')}}
+                    </label>
+                    <div class="col-md-7">                        
+                        <input name="current_password" class="form-control mt-4
+                            @error('current_password') is-invalid @enderror" 
+                            type="password" autocomplete = "current_password" required
+                        >
+                        @if(session()->has('msg'))
+                            <div class="alert alert-danger">
+                                {{ session()->get('msg') }}
+                            </div>
+                        @endif
+                        @if(session()->has('success'))
+                            <script>
+                                //alert ('Contraseña cambiada')
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'La contraseña se cambio exitosamente',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                    })
+                            </script>
+                        @endif
+                    </div>
+                    <label class="col-md-3 col-form-label  mt-4 " for="new_password" >{{__('Contraseña Nueva')}}</label>
+                    <div class="col-md-7">
+                        <input name="new_password" class="form-control mt-4
+                            @error('new_password') is-invalid @enderror" 
+                            type="password" autocomplete = "new_password" required
+                        >
+                        @error('new_password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div> 
+                    <label class="col-md-3 col-form-label mt-4 text-left" for="new_confirm_password" >
+                        {{__('Confirmar Contraseña')}}
+                    </label>
+                    <div class="col-md-7">
+                        <input name="new_confirm_password" class="form-control mt-4
+                            @error('new_confirm_password') is-invalid @enderror"
+                            type="password" autocomplete = "new_confirm_password" required
+                        >
+                        @error('new_confirm_password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                       
+                    </div>  
+                    <div>
+                        <div class="mt-2">
+                            <button type="submit" class="mt-4 btn btn-dark btn-ver">
+                                {{ __('Cambiar Contraseña') }}
+                            </button>                                    
+                        </div>
+                    </div>          
+                </div>
+            </form>
+        </div>
+          <div class="col-lg-6">
             <h2 class="fw-bold ">Edición usuario</h2>
             <form  method="POST" action="{{ route('change.user')}}">
                 @csrf
                 @method('PUT')
                 <div class="row mb-3 text-center mt-3">
-                    <label class="col-md-4 col-form-label mt-4 text-md-end" for="nombre_usuario" >
+                    <label class="col-md-4 col-form-label mt-4" for="nombre_usuario" >
                         {{__('Nombre de Usuario')}}
                     </label>
-                    <div class="col-md-6">
+                    <div class="col-md-7">
                         <input name="nombre_usuario" class="form-control mt-4
                             @error('nombre_usuario') is-invalid @enderror" 
                             type="text" autocomplete = "nombre_usuario" required
@@ -41,10 +102,10 @@
                         @enderror                    
                     </div>
                     
-                    <label class="col-md-4 col-form-label mt-4 text-md-end" for="telefono" >
+                    <label class="col-md-4 col-form-label mt-4" for="telefono" >
                         {{__('Telefono')}}
                     </label>
-                    <div class="col-md-6">
+                    <div class="col-md-7">
                         <input name="telefono" class="form-control mt-4
                             @error('telefono') is-invalid @enderror" 
                             type="text" autocomplete = "telefono"
@@ -72,77 +133,8 @@
                 </div>
             </form>
         </div>
-
-
-
-        <div class="row d-flex justify-content-center">
-          <div class="col-lg-8">
-            <h2 class="fw-bold ">Editar contraseña</h2>
-            <form  method="POST" action="{{ route('change.password')}}">
-                @csrf
-                <div class="row mb-3 text-center mt-3">
-                    <label class="col-md-4 col-form-label mt-4 text-md-end" for="current_password" >
-                        {{__('Contraseña Actual')}}
-                    </label>
-                    <div class="col-md-6">                        
-                        <input name="current_password" class="form-control mt-4
-                            @error('current_password') is-invalid @enderror" 
-                            type="password" autocomplete = "current_password" required
-                        >
-                        @if(session()->has('msg'))
-                            <div class="alert alert-danger">
-                                {{ session()->get('msg') }}
-                            </div>
-                        @endif
-                        @if(session()->has('success'))
-                            <script>
-                                //alert ('Contraseña cambiada')
-                                Swal.fire({
-                                    icon: 'success',
-                                    title: 'La contraseña se cambio exitosamente',
-                                    showConfirmButton: false,
-                                    timer: 1500
-                                    })
-                            </script>
-                        @endif
-                    </div>
-                    <label class="col-md-4 col-form-label  mt-4 text-md-end" for="new_password" >{{__('Contraseña Nueva')}}</label>
-                    <div class="col-md-6">
-                        <input name="new_password" class="form-control mt-4
-                            @error('new_password') is-invalid @enderror" 
-                            type="password" autocomplete = "new_password" required
-                        >
-                        @error('new_password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div> 
-                    <label class="col-md-4 col-form-label mt-4 text-md-end" for="new_confirm_password" >
-                        {{__('Confirmar Contraseña')}}
-                    </label>
-                    <div class="col-md-6">
-                        <input name="new_confirm_password" class="form-control mt-4
-                            @error('new_confirm_password') is-invalid @enderror"
-                            type="password" autocomplete = "new_confirm_password" required
-                        >
-                        @error('new_confirm_password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                       
-                    </div>  
-                    <div>
-                        <div class="mt-2">
-                            <button type="submit" class="mt-4 btn btn-dark btn-ver">
-                                {{ __('Cambiar Contraseña') }}
-                            </button>                                    
-                        </div>
-                    </div>          
-                </div>
-            </form>
-        </div>
+      </div>    
+        <div class="row">
             <div>
                 <h2 class="fw-bold mt-5 mb-5">Eliminar mi cuenta</h2>
                     <div class="row mb-3 text-center mt-3">
@@ -155,14 +147,7 @@
             </div>               
         </div>     
           </div>
-        </div>
-      </div>
     </div>
   </section>
-<div class="content-div2">
-    <div class="content-div" style ="background-color: #FFFFFF">
-            <div>
-            
-    </div>
-</div>
+    
 @endsection
